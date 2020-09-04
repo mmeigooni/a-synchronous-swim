6f80fe7c45e766f865c5bab9c8b88099c224a98e
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-09-04 16:29:11 -0700

    Set up first spec tests

--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -22,8 +22,12 @@ describe('server responses', () => {
   });
 
   it('should respond to a GET request for a swim command', (done) => {
-    // write your test here
-    done();
+    let {req, res} = server.mock('http://127.0.0.1:3000/', 'GET');
+    httpHandler.router(req, res, () => {
+      expect(res._responseCode).to.equal(200);
+      expect(res._ended).to.equal(true);
+      done();
+    });
   });
 
   xit('should respond with 404 to a GET request for a missing background image', (done) => {
